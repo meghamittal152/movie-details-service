b848d93ec3221e42934015126671078381bcf516
meghamittal152 <meghamittal152@gmail.com>
2021-01-01 19:24:58 +0100

    Initial commit

---
+++ src/main/java/com/test/moviedetailsservice/models/MovieSummary.java
@@ -0,0 +1,52 @@
+package com.test.moviedetailsservice.models;
+
+public class MovieSummary {
+	
+	private String movieId;
+	
+	private String title;
+
+	private String overview;
+	
+	public MovieSummary()
+	{
+		
+	}
+	
+	
+
+	public MovieSummary(String movieId, String title, String overview) {
+		super();
+		this.movieId = movieId;
+		this.title = title;
+		this.overview = overview;
+	}
+
+
+
+	public String getMovieId() {
+		return movieId;
+	}
+
+	public void setMovieId(String movieId) {
+		this.movieId = movieId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getOverview() {
+		return overview;
+	}
+
+	public void setMovieOverview(String overview) {
+		this.overview = overview;
+	}
+	
+	
+}
---
+++ src/test/java/com/test/moviedetailsservice/MovieDetailsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.moviedetailsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieDetailsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
